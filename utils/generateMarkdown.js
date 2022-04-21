b884b0666e33ef1f0da49a246446145488ea9a4e
KuyaDevin <devinnguyen@ucsb.edu>
2022-04-21 16:40:42 -0700

    Added table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,6 +14,20 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+*[Installation](#installation)
+*[Usage](#usage)
+*[License](#license)
+*[Contributing](#contributing)
+*[Tests](#tests)
+*[Questions](#questions)
+
+### Installation
+${data.installation}
+
 `;
 }
 
