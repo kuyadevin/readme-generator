15194790919fa98168eb87f06ee1db20a1f1e494
KuyaDevin <devinnguyen@ucsb.edu>
2022-04-21 15:12:48 -0700

    Added base files and created git ignore

---
+++ index.js
@@ -0,0 +1,21 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require("fs");
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project',
+        name: 'title'
+    }
+];
+
+// TODO: Create a function to write README file
+// fs append here
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
